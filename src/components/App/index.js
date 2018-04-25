fb2bb86f3159ceacac8b4c52288427829586f8f3
Warren Whipple <modalrealist@gmail.com>
2018-04-25 13:59:20 -0500

    Cleanup boilerplate

---
+++ src/components/App/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Pretty Wikivoyage</h1>
+        </header>
+        <p>Welcome to Pretty Wikivoyage.</p>
+      </div>
+    );
+  }
+}
+
+export default App;
