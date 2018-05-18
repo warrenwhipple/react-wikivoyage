dd30daa1b6712a211f3aa51f1310f732f763677b
Warren Whipple <modalrealist@gmail.com>
2018-05-18 01:11:41 -0500

    Route WikiPage html a local links

--- src/components/WikiPage/index.js
+++ src/components/WikiPage/index.js
@@ -3,11 +3,12 @@ import React from 'react';
 import DOMPurify from 'dompurify';
 import { wikiPage } from '../../wiki-fetch-cache';
 import { withRouter } from 'react-router-dom';
-import type { Location } from 'react-router-dom';
+import type { Location, RouterHistory } from 'react-router-dom';
 import './index.css';
 
 type Props = {
-  location: Location
+  location: Location,
+  history: RouterHistory
 };
 
 type State = {
@@ -38,6 +39,16 @@ class WikiPage extends React.Component<Props, State> {
     }
   };
 
+  handleClick = (event: Event) => {
+    const target = event.target;
+    if (target instanceof HTMLAnchorElement) {
+      if (target.hostname === window.location.hostname) {
+        event.preventDefault();
+        this.props.history.push(target.pathname);
+      }
+    }
+  };
+
   componentDidMount() {
     this.loadPageData();
   }
@@ -51,7 +62,7 @@ class WikiPage extends React.Component<Props, State> {
   render() {
     const { title, html } = this.state;
     return (
-      <div className="WikiPage">
+      <div className="WikiPage" onClick={this.handleClick}>
         <h1>{title}</h1>
         <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(html) }} />
       </div>
