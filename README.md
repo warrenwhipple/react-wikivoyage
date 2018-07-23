c51265e7420925aab31f88f140be34eeeecca320
Warren Whipple <modalrealist@gmail.com>
2018-07-23 12:32:58 -0400

    Add info to readme and landing page

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 A React client view for Wikivoyage.
 
 View the [demo](https://react-wikivoyage.netlify.com/).
+
+This is an early stage development project. It is missing features essential to a usable Wikivoyage client.
+
+## Working features
+
+* URL routing connected to the [English Wikivoyage API](https://en.wikivoyage.org/w/api.php)
+* Autosuggest search bar with [react-autosuggest](https://www.npmjs.com/package/react-autosuggest)
+* Wiki page caching with [localStorage](https://developer.mozilla.org/en-US/docs/Web/API/Storage/LocalStorage)
--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -1,12 +1,29 @@
 // @flow
 import React from 'react';
 import './index.css';
+import { Link } from 'react-router-dom';
 
 const LandingPage = () => (
   <div className="LandingPage">
     <h1 className="LandingPage__title">react-wikivoyage</h1>
     <div className="LandingPage__content">
-      A React client view for Wikivoyage.
+      <p>A React client view for Wikivoyage.</p>
+      <p>
+        Try any place on Earth in the search bar above. For example:{' '}
+        <Link to="/wiki/bogota">Bogotá</Link> or{' '}
+        <Link to="/wiki/madagascar">Madagascar</Link> or{' '}
+        <Link to="/wiki/antarctica">Antarctica</Link>
+      </p>
+      <p>
+        This is an early stage development project. It is missing features
+        essential to a usable Wikivoyage client.
+      </p>
+      <p>
+        View the{' '}
+        <a href="https://github.com/warrenwhipple/react-wikivoyage">
+          GitHub repo
+        </a>.
+      </p>
     </div>
   </div>
 );
